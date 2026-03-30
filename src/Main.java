e5257eb403a0df0093b41a4165ffcc8d9a177ed1
yangtori0407 <heyang236@gmail.com>
2026-03-30 23:24:49 +0900

    base code

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Pico pico = new Pico();
+
+        pico.printPico();
+    }
+}
---
+++ src/Pico.java
@@ -0,0 +1,10 @@
+public class Pico {
+    int health = 80;
+    int level = 0;
+
+    public void printPico(){
+        System.out.println("   ( •ө•)  ");
+        System.out.println("   (   > ) ");
+        System.out.println("    \"  \"   ");
+    }
+}
